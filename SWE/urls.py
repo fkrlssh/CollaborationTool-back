678c49a5f57cb429fae7cb7f5ad52c5f4d23cdf1
yanggang0525 <kjyvvdd@gmail.com>
2025-06-06 18:34:26 +0900

    각 앱 api 경로 연결, 파일 다운 및 미디어 접근 허용

--- SWE/urls.py
+++ SWE/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/test/", views.test_api),  
     path("api/", include("users.urls")),
-    
-    
+    path("api/projects/", include("projects.urls")),
+    path("api/tasks/", include("tasks.urls")),
+    path("api/files/", include("files.urls")),
+    path("api/comments/", include("comments.urls")),   
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
