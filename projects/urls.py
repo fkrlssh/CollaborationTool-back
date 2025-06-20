3c5ab675318a3ac1f28d80f1f47c0c1e90f80ced
yanggang0525 <kjyvvdd@gmail.com>
2025-06-20 16:27:01 +0900

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from projects.views.create_project_view import ProjectCreateView
 from projects.views.invite_member_view import InviteMemberView
 from projects.views.delete_project_view import ProjectDeleteView
-
+from projects.views.project_list_view import ProjectListView
+from projects.views.project_detail_view import ProjectDetailView
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='project-create'),
     path('<int:project_id>/invite/', InviteMemberView.as_view(), name='project-invite'),
     path('<int:project_id>/delete/', ProjectDeleteView.as_view(), name='project-delete'),
     path('', ProjectCreateView.as_view(), name='create-project'),
+    path('', ProjectListView.as_view(), name='project-list'),
+    path('<int:project_id>/', ProjectDetailView.as_view(), name='project-detail'),
 ]
